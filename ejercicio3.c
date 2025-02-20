a7ed3fb5e7a6b659b9bd60c1f9f6df25062e994f
Cordova Villar Mario Alberto <mariocv670@gmail.com>
2025-02-20 10:52:14 -0600

    Archivos trabajados en clase

--- ejercicio3.c
+++ ejercicio3.c
@@ -8,19 +8,20 @@ int main () {
   int nums[3][3] = {{99,88,77},
                     {66,55,44},
                     {33,22,11}};
-  ap = nums;
+  ap = &nums[0][0];
   // Se puede recorrer un arreglo usando solo un ciclo for
   for (int i = 0; i < 9 ; i++){
     if ((i%3)==0)
       printf("\n");
-    printf("%x\t",(ap+i));
+    printf("%x %d\t",(ap+i));
     //imprime el valor de la dirección del elemento mostrado usando el ap e i
+    printf("%x\t",(ap+i));
   }
 
   for (int i = 0; i < 3 ; i++){
-    for(int j = 0: j< 3; j++){
-      printf("%x\t",(ap+indice));
-      //imprime el valor de la dirección del elemento mostrado usando el ap, i y j
+    for(int j = 0; j< 3; j++){
+      printf("%x %d\t",(ap+i*3+j), *(ap+i*3+j));  //el 3 es el numero de elementos de cada renglon
+      //imprime el valor de lac dirección del elemento mostrado usando el ap, i y j
     }
     printf("\n");
   }
